0d314bbce0bafcd68a7c4b8d12ce44acb55a1810
Deji <68882568+deji100@users.noreply.github.com>
2023-06-27 15:49:37 +0100

    Update tel.py

--- telnyx/tel.py
+++ telnyx/tel.py
@@ -1,6 +1,6 @@
 import telnyx
 
-telnyx.api_key = "KEY0185F3DFB6FF8BA37377156B52E04A3C_FWXTWIHjRBjZaNUOtiby0U"
+telnyx.api_key = ""
 
 telnyx.NumberOrder.create(
   phone_numbers=[{"phone_number": "+18722165438"}]
